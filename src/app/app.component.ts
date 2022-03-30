8eb5d39da9f2cbbd483e3b08239e6f5b4c71ee7d
MatteoR1994 <matteo.rinaldi1893@gmail.com>
2022-03-30 10:00:40 +0200

    memorie del browser

---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'angularTest';
+
+  user = { name: 'andrea', loginDate: new Date(), results: [100, 1002, 1] };
+
+  user2?: any;
+
+  ngOnInit(): void {
+    // SESSION STORAGE
+    // window.sessionStorage.setItem('ciao', 'mondo');
+
+    // LOCAL STORAGE
+    // window.localStorage.setItem('pippo', 'pluto');
+    // console.log(window.localStorage.getItem('pippo'));
+    window.localStorage.setItem('pippo', JSON.stringify(this.user));
+    // console.log(JSON.parse(window.localStorage.getItem('pippo')!));
+    if (window.localStorage.getItem('pippo') !== null) {
+      this.user2 = JSON.parse(window.localStorage.getItem('pippo') as any);
+    }
+
+    // COOKIES
+    const key = 'name';
+    const value = 'simone';
+    const date = new Date();
+    date.setFullYear(date.getFullYear() + 1);
+
+    const cookie = key + '=' + value + ';expires=' + date.toUTCString() + ';path=/;SameSite=Lax';
+    document.cookie = cookie;
+
+    const cookie2 = 'pippo' + '=' + 'pluto' + ';expires=' + date.toUTCString() + ';path=/;SameSite=Lax';
+    document.cookie = cookie2;
+
+    console.log(document.cookie);
+    
+  }
+
+}
