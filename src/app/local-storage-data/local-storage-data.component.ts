eb1c4449975d4cecacf67be0e7cf11e5252333cc
MatteoR1994 <matteo.rinaldi1893@gmail.com>
2022-03-30 11:28:21 +0200

    Update local-storage-data.component.ts

--- src/app/local-storage-data/local-storage-data.component.ts
+++ src/app/local-storage-data/local-storage-data.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-local-storage-data',
@@ -17,9 +17,23 @@ export class LocalStorageDataComponent implements OnInit {
     }
   }
 
+  //@Input() userChanged: boolean = false;
+
   constructor() { }
+
+  // ngOnChanges(changes: SimpleChanges): void {
+  //   this.displayData();
+  // }
 
   ngOnInit(): void {
   }
+
+  // displayData() {
+  //   if (this.userChanged) {
+  //     if (window.localStorage.getItem('local') !== null) {
+  //       this.user = JSON.parse(window.localStorage.getItem('local') as any);
+  //     }
+  //   }
+  // }
 
 }
